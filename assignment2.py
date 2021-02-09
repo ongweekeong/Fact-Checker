3c69e4d3ab371b6aba8572bcab7f607a399a8f70
Ong Wee Keong <35793358+ongweekeong@users.noreply.github.com>
2021-02-09 14:06:41 +0800

    Skeleton file and data

---
+++ assignment2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python.
+
+"""
+CS4248 ASSIGNMENT 2 Template
+
+TODO: Modify the variables below.  Add sufficient documentation to cross
+reference your code with your writeup.
+
+"""
+
+# Import libraries.  Add any additional ones here.
+# Generally, system libraries precede others.
+import pandas as pd
+from sklearn.metrics import f1_score
+
+# TODO: Replace with your Student Number
+_STUDENT_NUM = 'A01234567X'
+
+def train_model(model, X_train, y_train):
+    ''' TODO: train your model based on the training data '''
+    pass
+
+def predict(model, X_test):
+    ''' TODO: make your prediction here '''
+    return [0]*len(X_test)
+
+def generate_result(test, y_pred, filename):
+    ''' generate csv file base on the y_pred '''
+    test['Verdict'] = pd.Series(y_pred)
+    test.drop(columns=['Text'], inplace=True)
+    test.to_csv(filename, index=False)
+
+def main():
+    ''' load train, val, and test data '''
+    train = pd.read_csv('train.csv')
+    X_train = train['Text']
+    y_train = train['Verdict']
+    model = None # TODO: Define your model here
+
+    train_model(model, X_train, y_train)
+    # test your model
+    y_pred = predict(model, X_train)
+
+    # Use f1-macro as the metric
+    score = f1_score(y_train, y_pred, average='macro')
+    print('score on validation = {}'.format(score))
+
+    # generate prediction on test data
+    test = pd.read_csv('test.csv')
+    X_test = test['Text']
+    y_pred = predict(model, X_test)
+    generate_result(test, y_pred, _STUDENT_NUM + ".csv")
+
+# Allow the main class to be invoked if run as a file.
+if __name__ == "__main__":
+    main()
