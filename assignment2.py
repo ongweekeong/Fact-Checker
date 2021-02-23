4cc1ad9d720a92f0ff38fb30211133299c4d226b
ongweekeong <ongweekeong@gmail.com>
2021-02-23 20:32:12 +0800

    Preprocessing function done

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python.
 
 """
-CS4248 ASSIGNMENT 2 Template
+CS4248 ASSIGNMENT 2
 
 TODO: Modify the variables below.  Add sufficient documentation to cross
 reference your code with your writeup.
@@ -11,8 +11,26 @@ reference your code with your writeup.
 # Import libraries.  Add any additional ones here.
 # Generally, system libraries precede others.
 import pandas as pd
+import numpy as np
 from sklearn.metrics import f1_score
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+import gensim
+from gensim.parsing.preprocessing import remove_stopwords
+
+from tensorflow.keras import models, layers, preprocessing as kprocess
+from tensorflow.keras import backend as K
+
+nltk.download('punkt')
+nltk.download('stopwords')
+STOPWORDS = stopwords.words('english')
 _STUDENT_NUM = 'A0168932U'
 
 def train_model(model, X_train, y_train):
@@ -20,7 +38,7 @@ def train_model(model, X_train, y_train):
     pass
 
 def predict(model, X_test):
-    ''' TODO: make your prediction here '''
+    ''' TODO: make predictions here '''
     return [0]*len(X_test)
 
 def generate_result(test, y_pred, filename):
@@ -29,12 +47,23 @@ def generate_result(test, y_pred, filename):
     test.drop(columns=['Text'], inplace=True)
     test.to_csv(filename, index=False)
 
+def preprocess(X_train):
+    ''' Remove stop words, punctuations, and do normalizations '''
+    X_train_clean = []
+    for sentence in X_train:
+        tokens = []
+        tokens += word_tokenize(sentence.lower())
+        words = [word for word in tokens if (word.isalpha() and not word in STOPWORDS)]
+        sentence = (" ").join(words)
+        X_train_clean.append(sentence)
+    return pd.Series(X_train_clean)
+
 def main():
     ''' load train, val, and test data '''
     train = pd.read_csv('train.csv')
-    X_train = train['Text']
+    X_train = preprocess(train['Text'])
     y_train = train['Verdict']
-    model = None # TODO: Define your model here
+    model = None # TODO: Define model
 
     train_model(model, X_train, y_train)
     # test your model
